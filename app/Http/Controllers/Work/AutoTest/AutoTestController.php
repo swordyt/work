b0db770a9e98fbb8d9003ddc513e57e380f4922f
unknown <YINTING933@PAFSH-D0259.paicdom.local>
2016-01-20 18:07:36 +0800

    hotfix1.0

--- app/Http/Controllers/Work/AutoTest/AutoTestController.php
+++ app/Http/Controllers/Work/AutoTest/AutoTestController.php
@@ -14,20 +14,19 @@ class AutoTestController extends Controller {
 
 	}
 	public function getModrequestdata(Request $request){
-		$filePath = $this->getDataPath($request->id);
-		$reader = Excel::load($filePath);
-		$data = $reader->all();
-		
+		$data=$this->LoadDataByID($request->input('id'));
+		dd('s');
 		$req=array();
 		$number=0;
-		for ($i=1; $i < sizeof($data); $i++) { 
+		$req[$number++]=array_keys($data[0]);
+		for ($i=1; $i < sizeof($data)+1; $i++) { 
 			if($i == $request->input('row')){
 				continue;
 			}
-			$req[$number++]=array_values($data[$i]);
+			$req[$number++]=array_values($data[$i-1]);
 		}
 		$this->SaveData($request->input('id'),$req);
-		// return $this->getRequest($request->input('id'));
+		return $this->getRequest($request->input('id'));
 
 	}
 	private function getDataPath($id){
@@ -44,14 +43,12 @@ class AutoTestController extends Controller {
 	}
 	private function SaveData($id,$data){
 		$reqEntity = InterRequest::find($id);
-		// $path='rm '.storage_path('excel').'/'.$reqEntity->name.'.xlsx';
-		// 	`$path`;
-		Excel::create('data',function($excel) use($data,$reqEntity){
+		//unlink(storage_path('excel').'/'.$reqEntity->name.'.xlsx');
+		Excel::create($reqEntity->dataaddress,function($excel) use($data,$reqEntity){
 			$excel->sheet('data',function($sheet) use($data){
-				// dd($data);
-				$sheet->fromArray($data);
+				$sheet->rows($data);
 			});
-		})->store('xls',storage_path('excel'));
+		})->store('xlsx',storage_path('excel'));
 	}
 	public function getShowrequest(){
 		$path='rm '.storage_path('excel').'/login.xlsx';
